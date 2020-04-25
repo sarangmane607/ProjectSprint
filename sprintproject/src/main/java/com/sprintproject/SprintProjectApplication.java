06533ba188dbc05354e6751012f7cc7376ceef62
sarangmane607 <62138683+sarangmane607@users.noreply.github.com>
2020-04-25 16:36:32 +0530

    new files
    new files

---
+++ sprintproject/src/main/java/com/sprintproject/SprintProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sprintproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintProjectApplication.class, args);
+	}
+
+}
